12734efe886c7cf8945228903d5ab289d46c7373
stdev0617 <stdev0617@gmail.com>
2021-04-08 00:23:49 +0900

    added solution to use hashmap

---
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,32 @@
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.equals("")) {
+            return 0;
+        }
+        StringBuilder temp = new StringBuilder();
+        int result = 1;
+//        for(int i = 0; i<s.length();i++){
+//            if(temp.toString().contains(String.valueOf(s.charAt(i)))) {
+//                result = Math.max(result, temp.length());
+//                temp.setLength(0);
+//            }
+//            temp.append(s.charAt(i));
+//            if(i == s.length()-1) {
+//                result = Math.max(result, temp.length());
+//            }
+//        }
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = i; j < s.length(); j++) {
+                if(temp.toString().contains(String.valueOf(s.charAt(j)))) {
+                    result = Math.max(result, temp.length());
+                    temp.setLength(0);
+                }
+                temp.append(s.charAt(j));
+                if(j == s.length()-1) {
+                    result = Math.max(result, temp.length());
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/ValidSudoku.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+
+        HashMap<Integer, Integer> []rows = new HashMap[9];
+        HashMap<Integer, Integer> []cols = new HashMap[9];
+        HashMap<Integer, Integer> []boxes = new HashMap[9];
+
+        for(int i = 0; i<9;i++) {
+            rows[i] = new HashMap<Integer, Integer>();
+            cols[i] = new HashMap<Integer, Integer>();
+            boxes[i] = new HashMap<Integer, Integer>();
+        }
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if(num != '.') {
+                    int n = (int) num;
+                    int box_index = (i/3)*3 + j/3;
+
+                    rows[i].put(n, rows[i].getOrDefault(i, 0)+1);
+                    cols[i].put(n, cols[i].getOrDefault(i, 0)+1);
+                    boxes[box_index].put(n, boxes[box_index].getOrDefault(i, 0)+1);
+                    if(rows[i].get(n) > 1 || cols[i].get(n) > 1 || boxes[box_index].get(n) > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
